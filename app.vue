4b49258b7ea74391e8a1095f21163e9fcc913a76
Chaloulou75 <c.jeandey@gmail.com>
2023-01-26 13:51:51 +0100

    second commit

--- app.vue
+++ app.vue
@@ -1,9 +1,16 @@
 <template>
-  <div class="flex items-center w-full min-h-screen mx-auto bg-blue-300">
+  <div
+    class="flex flex-col items-center justify-around w-full min-h-screen mx-auto bg-blue-500"
+  >
     <h1
-      class="mx-auto text-3xl font-bold text-center text-blue-800 underline md:text-7xl max-w-7xl"
+      class="mx-auto text-3xl font-bold text-center text-gray-500 underline md:text-7xl max-w-7xl"
     >
-      Farid, votre transporteur en IDF
+      Coursier Discount
     </h1>
+    <h2
+      class="mx-auto text-3xl font-bold text-center text-white md:text-7xl max-w-7xl"
+    >
+      Livraison sur toute l'Ile de France
+    </h2>
   </div>
 </template>
