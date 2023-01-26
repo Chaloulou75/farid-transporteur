05bd58f3813ab4fc3e89148ee35b26fe98dd8b26
Chaloulou75 <c.jeandey@gmail.com>
2023-01-26 13:08:22 +0100

    initial commit

---
+++ app.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="flex items-center w-full min-h-screen mx-auto bg-blue-300">
+    <h1
+      class="mx-auto text-3xl font-bold text-center text-blue-800 underline md:text-7xl max-w-7xl"
+    >
+      Farid, votre transporteur en IDF
+    </h1>
+  </div>
+</template>
---
+++ nuxt.config.ts
@@ -0,0 +1,25 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  app: {
+    head: {
+      charset: "utf-8",
+      viewport: "width=device-width, initial-scale=1",
+      title: "Coursier Discount | Livraison sur toute l'Ile de France",
+      meta: [
+        {
+          name: "description",
+          content:
+            "Livraison sur toute l'Ile de France, vos courses à petits prix sur Paris et la Région Parisienne. Coursier Discount.",
+        },
+      ],
+    },
+    pageTransition: { name: "page", mode: "out-in" },
+  },
+  css: ["~/assets/css/tailwind.css"],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+});
